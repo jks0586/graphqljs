1ad176031b2a4efa3e194872e91072a7c9d7a384
Jitendra <jks0586@gmail.com>
2022-07-19 18:34:52 +0530

    'upate'

--- api/src/config.js
+++ api/src/config.js
@@ -1,4 +1,3 @@
 // export const MONGODB_URL = "mongodb+srv://letscms:letscms@1243@cluster0.rirwt.mongodb.net/graphqldb?retryWrites=true&w=majority"
 export const MONGODB_URL = "mongodb://localhost:27017/graphqldb"
-
 export const JWT_SECRET ="inkarggg"
